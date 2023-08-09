e784d487f36f8f78d0866c8f62515ae81378078f
eseulssi <eeeeeseulllll@naver.com>
2023-08-09 17:49:48 +0900

    start

--- src/main/java/com/example/WorldChatProject/cateChat/MessageType.java
+++
@@ -1,8 +0,0 @@
-package com.example.WorldChatProject.cateChat;
-
-public enum MessageType {
-    CHAT,
-    JOIN,
-    LEAVE
-
-}
--- src/main/java/com/example/WorldChatProject/cateChat/repository/CateFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.WorldChatProject.cateChat.repository;
-
-import com.example.WorldChatProject.cateChat.entity.CateFile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CateFileRepository extends JpaRepository<CateFile, Long>  {
-}
