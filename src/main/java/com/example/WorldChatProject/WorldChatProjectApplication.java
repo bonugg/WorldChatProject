b0c50b829e944197831cb2e78bd29b389f4bafc1
eseulssi <eeeeeseulllll@naver.com>
2023-08-24 10:45:47 +0900

    정보변경

--- src/main/java/com/example/WorldChatProject/WorldChatProjectApplication.java
+++ src/main/java/com/example/WorldChatProject/WorldChatProjectApplication.java
@@ -1,7 +1,9 @@
 package com.example.WorldChatProject;
 
+import com.example.WorldChatProject.cateChat.controller.TranslationController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WorldChatProjectApplication {
